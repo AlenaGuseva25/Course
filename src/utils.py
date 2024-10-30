d543e60deefaa36977776ccf3b6834d0535dbc3a
Alena <alittlekindness94@gmail.com>
2024-10-30 15:22:29 +0500

    cards

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import datetime
 import logging
 import json
+import pandas as pd
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -28,9 +29,55 @@ if __name__ == "__main__":
     print(greeting_message)
 
 
-def set_cards_dicts():
+def set_cards_dicts(exsel_file_path):
     """Функция формирует словарь по картам пользователя и вносит значение по ключу cards"""
-    pass
+    logging.info("Обработка файла: %d", exsel_file_path)
+
+    try:
+        df = pd.read_excel(exsel_file_path)
+        logging.info("Файл прочитан. Найдено %d строк.", len(df))
+    except Exception as e:
+        logging.error("Ошибка чтения файла: %d", e)
+        return {"error": "Ошибка чтения файла"}
+
+    cards_summary = []
+
+    for index, row in df.iterrows():
+        try:
+            last_digits = str(row["Номер карты"])[-4:]
+            total_spent = float(row["Сумма операции"])
+            cashback = total_spent / 100.0
+
+            cards_info = {
+                "last_digits": last_digits,
+                "total_spent": total_spent,
+                "cashback": cashback
+            }
+
+            cards_summary.append(cards_info)
+            logging.debug("Добавлена информация о карте: %d", cards_info)
+
+
+        except KeyError as e:
+            logging.error("Ошибка: отсутствует столбец %s в строке %d", e, index)
+        except Exception as e:
+            logging.error("Ошибка при обработке строки %d: %s", index, e)
+
+
+    logging.info("Обработка завершена. Найдено %d карт.", len(cards_summary))
+    return {"cards": cards_summary}
+
+
+if __name__ == "__main__":
+    excel_file_path = (r"C:\Users\Alena\my_1\Course_paper\data\operations.xlsx")
+
+    summary = set_cards_dicts(excel_file_path)
+    print(summary)
+
+
+
+
+
 
 
 def set_five_trans_dicts():
