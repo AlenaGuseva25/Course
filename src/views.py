785a95474867bc2d6c290dfe1e6b4f2a13bbdd9c
Alena <alittlekindness94@gmail.com>
2024-10-31 18:29:22 +0500

    pravki flake8

--- src/views.py
+++ src/views.py
@@ -9,20 +9,14 @@ def main(date_time_str: str, excel_file_path: str) -> str:
     """Главная функция, принимающая дату и время, и возвращающая JSON-ответ"""
     response = {}
 
-    # Установка приветствия
     response["greeting"] = set_greeting()
 
-    # Получение информации о картах
     response["cards"] = set_cards_dicts(excel_file_path)
 
-    # Получение топ-5 транзакций
     response["top_transactions"] = set_five_trans_dicts(excel_file_path)
 
-    # Получение курсов валют
     response["currency_rates"] = set_currency_rates_dicts({"currency_rates": []})
 
-    # Получение цен акций
     response["stock_prices"] = stock_prices()
 
-    # Преобразование в JSON
     return json.dumps(response, ensure_ascii=False, indent=4)
